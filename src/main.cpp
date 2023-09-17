718526db6cd8dc28709570660b727131d2b72c3a
Miro Palmu <miro.palmu@helsinki.fi>
2023-09-17 17:09:15 +0300

    Initialize project

--- src/main.cpp
+++ src/main.cpp
@@ -1,8 +1,20 @@
 #include <iostream>
+#include <span>
+#include <string_view>
+#include <vector>
 
-#include "foo.hpp"
+auto main(int argc, char const* const* const argv) -> int {
+    [[nodiscard]] int better_main(std::span<const std::string_view>) noexcept;
 
-auto main([[maybe_unused]] int argc,[[maybe_unused]] char** argv) -> int {
+    auto args = std::vector<std::string_view>(
+        argv, std::next(argv, static_cast<std::ptrdiff_t>(argc)));
 
-	std::cout << foo::bar() << "\n";
+    return better_main(args);
+}
+
+[[nodiscard]] int better_main(std::span<const std::string_view> args) noexcept {
+
+    const auto 
+
+    return 0;
 }
